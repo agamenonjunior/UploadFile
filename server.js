df6d037e5eab8f6e1c1d1d0c156f871f041fea51
agamenonjunior <agamenon_junior1@hotmail.com>
2021-11-28 20:01:48 -0300

    adicionando o multer

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const app = express()
+const path = require('path')
 
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -9,6 +10,15 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 
+const armazenamento = multer.diskStorage({
+    destination:(req,file,callBack)=>{
+        callBack(null,'/upload')
+    },
+    filename:(req,file,callBack)=>{
+        //Nome do arquivo + data agora + extensao 
+        callBack(null,file.fieldname+'-'+Date.now()+path.extname(file.originalname))
+    }
+})
 
 app.listen(3000,'127.0.0.1',()=>{
     console.log('server running...')
