17b73166b62a47826700cb0db78c7f01a4bffd29
agamenonjunior <agamenon_junior1@hotmail.com>
2021-12-01 20:42:04 -0300

    melhorias no server

--- server.js
+++ server.js
@@ -3,27 +3,29 @@ const bodyParser = require('body-parser')
 const multer = require('multer')
 const app = express()
 const path = require('path')
-
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-
+//armazenando no disco
 const armazenamento = multer.diskStorage({
     destination:(req,file,callBack)=>{
-        callBack(null,'')
+        callBack(null,'./upload')
     },
     filename:(req,file,callBack)=>{
         //Nome do arquivo + data agora + extensao 
+        let a = file.fieldname+'-'+Date.now()+path.extname(file.originalname)
         callBack(null,file.fieldname+'-'+Date.now()+path.extname(file.originalname))
+        console.log( a)
     }
 })
 
 const upload = multer({armazenamento})
-
+//rota padrao
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 
+//rota reposável pelo upload do arquivo
 app.post('/upload', upload.single('arquivo'),(req, res, next)=>{
     const file = req.file
     res.send(file)
@@ -31,9 +33,9 @@ app.post('/upload', upload.single('arquivo'),(req, res, next)=>{
         const error = new Error('Favor selecionar um arquivo')
         error.httpStatusCode = 400
         return next(error)
+    }else{
+        console.log("Ok")
     }
-    
-
 })
 
 
